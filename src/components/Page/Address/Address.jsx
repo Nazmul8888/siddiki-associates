1b662004f2568ae58792f481d4d4edd0c3bb0dc4
Nazmul8888 <nazmulkhan416@gmail.com>
2024-01-14 18:15:54 +0600

    some issue are update

--- src/components/Page/Address/Address.jsx
+++ src/components/Page/Address/Address.jsx
@@ -1,9 +1,12 @@
+import Container from "../../Hooks/Container";
 
 
 const Address = () => {
     return (
+        <div className=" bg-[#4b1e098a]">
+            <Container>
         <div className=" flex justify-evenly items-center">
-          <div className=" text-[#FFFFE4] shadow font-[14px] p-10 m-5 bg-red-500">
+          <div className=" text-[#FFFFE4] shadow font-[14px] p-10 m-5 bg-red-500 font-mono">
             <h1>Chamber</h1>
             <ul >
                 <li >Suit # 12-J+H (12th Floor),
@@ -14,7 +17,7 @@ const Address = () => {
                 
             </ul>
             </div>  
-          <div  className=" text-[#FFFFE4] shadow font-[14px] p-10 m-5 bg-red-500 ">
+          <div  className=" text-[#FFFFE4] shadow font-[14px] p-10 m-5 bg-red-500 font-mono ">
             <h1>Chamber</h1>
             <ul>
                 <li>Suit # 12-J+H (12th Floor),
@@ -25,6 +28,42 @@ const Address = () => {
                 
             </ul>
             </div>  
+        </div>
+        <div className=" flex justify-evenly items-center">
+          <div className=" mr-40 ">
+                <img  src="https://i.ibb.co/g4NjwBX/Screenshot-2024-01-14-122822.png" alt="" />  
+            </div>  
+            <form  className=" " >
+                <h1 className=" text-5xl font-bold font-mono">Ask a Question</h1>
+            <div className="md:flex mb-8 ">
+                <div className="form-control md:w-1/2">
+                    <label className="label">
+                      
+                    </label>
+                    <label className="input-group">
+                        
+                        <input type="text" name="name"  placeholder="Your Name" className="input input-bordered w-full" />
+                    </label>
+                </div>
+                <div className="form-control md:w-1/2 ml-4">
+                    <label className="label">
+                        
+                    </label>
+                    <label className="input-group">
+                        
+                        <input type="text" name="email" placeholder="Your Email" className="input input-bordered w-full" />
+                    </label>
+                </div>
+                </div>
+                <div><textarea className="textarea textarea-info w-96" placeholder="Message"></textarea></div>
+                <div><button className="btn btn-info">SEND</button></div>
+                </form>
+                
+        </div>
+        </Container>
+        <br />
+        <br />
+
         </div>
     );
 };
